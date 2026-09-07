8c098a8c52a963613fa5a69d0110643fb40249ee
ritiksundaram <sundaramritik@gmail.com>
2026-09-07 14:38:36 -0500

    fix ring buffer implementation

--- ring_buffer/ring_buffer.cpp
+++ ring_buffer/ring_buffer.cpp
@@ -1,54 +1,73 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <optional>
+#include <iostream>
 using namespace std;
-
-class Template <T> RingBuffer{
-
-private:
-    size_t tail = 0;
+template <typename T>
+class RingBuffer{
+    private:
     size_t head = 0;
-    size_t count = 0;
+    size_t tail = 0;
+    size_t current = 0;
     vector<T> buf;
-
-public:
-RingBuffer(size_t capacity) : buf(capacity); {}
-
-int size(){
-    return count;
-}
-
-bool empty(){
-    return count == 0;
-}
-
-bool full(){
-    return count == buf.size();
-}
-
-bool push(T& item){
-     if (full()){
-        return false;
-     }
-     buf[tail] = item; 
-     tail = (tail + 1) % buf.size();
-     return true;
-}
-
-optional<T> pop(){
-    if (empty()){
-        return nullopt;
-    }
-    T item = buf[head];
-    head = (head+1) % buf.size();
-    return item;
-}
-
-optional<T> peak(){
-    if (empty()){
-    return nullopt;
-    }
-    T item = buf[head];
-    return item;
-}
-
-
     
+    public:
+    explicit RingBuffer(size_t capacity) : buf(capacity) {}
+    
+    size_t size() const{
+        return current;
+    }
+    bool empty() const{
+        if (current==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    bool full() const{
+        if (current == buf.size()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    bool push(const T& item){
+        if (full()){
+            return false;
+        }
+        buf[tail] = item;
+        tail = (tail+1) % buf.size();
+        current++;
+        return true;
+    }
+    optional<T> pop(){
+        if (empty()){
+            return nullopt;
+        }
+        T item = buf[head];
+        head = (head+1) % buf.size();
+        current--;
+        return item;
+    }
+    optional<T> peek() const{
+        if (empty()){
+            return nullopt;
+        }
+        return buf[head];
+    }
+};
+
+int main(){
+    RingBuffer<int> rb(3);
+    rb.push(3);
+    rb.push(5);
+    rb.push(7);
+    cout << rb.full() << "\n";
+    cout << rb.empty() << "\n";
+    cout << rb.push(9) << "\n";
+    cout << *rb.pop() << " " << rb.size() << "\n";
+    auto x = rb.pop();
+    cout << (x ? "has value" : "empty") << "\n";
+
+}
